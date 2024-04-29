04a75181629792ef8ed6dc50b279b76f54c6de5a
jwfung <59128478+jwfung@users.noreply.github.com>
2024-04-29 01:34:49 -0400

    completed functionality for one round

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
-console.log("hello world")
+
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+let humanScore = 0
+let computerScore = 0
+
+playRound(humanChoice,computerChoice);
+
+function getComputerChoice() {
+  const choice = Math.floor(Math.random() * 3)
+  switch(choice) {
+    case 0:
+      return "ROCK";
+    case 1:
+      return "PAPER";
+    case 2:
+      return "SCISSORS"
+  }
+}
+
+function getHumanChoice() {
+  let choice = prompt("Rock, Paper, Scissors?")
+  return choice.toUpperCase();
+}
+
+function playRound(humanChoice,computerChoice) {
+  let win = false;
+
+  if (humanChoice == "ROCK") {
+    if (computerChoice == "PAPER") {
+      computerScore++;
+    }
+    else if (computerChoice == "SCISSORS") {
+      humanScore++;
+      win = true;
+    }
+  } else if (humanChoice == "PAPER") {
+    if (computerChoice == "SCISSORS") {
+      computerChoice++;
+    }
+    else if (computerChoice == "ROCK") {
+      humanChoice++;
+      win = true;
+    }
+  } else if (humanChoice == "SCISSORS") {
+    if (computerChoice == "ROCK") {
+      computerChoice++;
+    }
+    else if (computerChoice == "PAPER") {
+      humanChoice++;
+      win = true
+    }
+  } else {
+    console.log("It's a tie!")
+    return
+  }
+  
+  win ? console.log(`You win! ${humanChoice} beats ${computerChoice}`) : console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
+
+}
